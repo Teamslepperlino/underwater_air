4e30f9f0c79ad745e873056f65c23ae102f9efef
Teamslepperlin0 <97560968+Teamslepperlino@users.noreply.github.com>
2025-07-14 22:27:20 +0200

    config.lua erstellen

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {
+    OxygenTime = 20 * 60, -- Standard: 20 Minuten in Sekunden
+    NotifySystem = "esx", -- "esx" oder "okok"
+    Inventory = "ox",     -- "ox" oder "esx"
+    Debug = true,
+    Language = "de"       -- "de", "en", "it", "fr"
+}
