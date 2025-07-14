514eb08e30b50f21d3b2712a11fae470d8c8720d
Teamslepperlin0 <97560968+Teamslepperlino@users.noreply.github.com>
2025-07-14 22:25:58 +0200

    fxmanifest.lua erstellen

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Teamslepperlin0'
+description 'Diving System for your ESX Server'
+version '1.2.0'
+
+shared_scripts {
+    'config.lua',
+    'languages.lua'
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
